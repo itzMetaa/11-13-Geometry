e9337cc6588d745a78ccca56092ac0182396c491
Diak <Diak@10.4.117.15>
2019-11-13 14:18:20 +0100

    init

---
+++ src/geometry/GeneralRhombus.java
@@ -0,0 +1,28 @@
+
+package geometry;
+
+
+public class GeneralRhombus extends Rhombus{
+    public double a;
+    public double alfa;
+
+    public GeneralRhombus(double a, double alfa) {
+        this.a = a;
+        this.alfa = alfa;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getAlfa() {
+        return alfa;
+    }
+
+    @Override
+    public String toString() {
+        return "GeneralRhombus{" + "a=" + a + ", alfa=" + alfa + '}';
+    }
+    
+    
+}
